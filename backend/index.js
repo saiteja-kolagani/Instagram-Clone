7e3b16131a16ef50c7eb9165f3d4fc011f87349b
SaiTejaKolagani <saiteja.kolagani@gmail.com>
2024-10-23 21:20:37 +0530

    Cors Changes

--- backend/index.js
+++ backend/index.js
@@ -16,17 +16,16 @@ const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
-//middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
+
 const corsOptions = {
     origin: process.env.URL,
     credentials: true
 }
 app.use(cors(corsOptions));
 
-// yha pr apni api ayengi
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
--- frontend/src/dbPath/index.js
+++ frontend/src/dbPath/index.js
@@ -1,2 +1,4 @@
 export const dbPath = "http://localhost:4000";
+// export const dbPath = "https://instagram-clone-736u.onrender.com";
 export const socketPath = "http://localhost:4000"
+// export const socketPath = "wss://instagram-clone-736u.onrender.com";
