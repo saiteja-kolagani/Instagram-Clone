46e0faf82584d36f74869df3a53b5b6c4e88b9a3
SaiTejaKolagani <saiteja.kolagani@gmail.com>
2024-11-10 19:55:57 +0530

    In Production

--- frontend/src/dbPath/index.js
+++ frontend/src/dbPath/index.js
@@ -1,4 +1,4 @@
-export const dbPath = "http://localhost:4000";
-export const socketPath = "http://localhost:4000"
-// export const dbPath = "https://instagram-clone-5l2n.onrender.com";
-// export const socketPath = "wss://instagram-clone-5l2n.onrender.com";
+// export const dbPath = "http://localhost:4000";
+// export const socketPath = "http://localhost:4000"
+export const dbPath = "https://instagram-clone-5l2n.onrender.com";
+export const socketPath = "wss://instagram-clone-5l2n.onrender.com";
